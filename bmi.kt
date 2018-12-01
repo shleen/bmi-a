88e628f43a063d9a3c12d2cb97151e3cf1ee4bfd
Kaioru <keifuurawrs@gmail.com>
2018-12-01 15:16:31 +0800

    Add bmi.kt

---
+++ bmi.kt
@@ -0,0 +1,12 @@
+import kotlin.math.pow
+
+fun main(args: Array<String>) {
+    println("Find out your bmi here! Now! (only if u use metric units)")
+    print("Your Weight (kg):")
+    val weight = readLine()?.toFloat()!!
+    print("Your Height (m):")
+    val height = readLine()?.toFloat()!!
+
+    val bmi = weight / (height.pow(2))
+    println("We have a winner! Your BMI is %.2f.".format(bmi))
+}
