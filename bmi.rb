9be420891a321d081b50bbc7f87775938d4d590f
Zach <s10179991@connect.np.edu.sg>
2018-12-01 12:48:27 +0800

    updated bmi.rb
    fix a few indentation and spacing errors

--- bmi.rb
+++ bmi.rb
@@ -25,19 +25,19 @@ case condition
     puts "Please either grow #{net_height.round(3)}m or lose #{net_weight.round(3)}kg."
     puts 
     calories = net_weight * 1000 * 9
-    puts "The recommended daily calorie intake for you is #{rec_modactive_calorie} calories assuming a moderately       active lifestyle."
+    puts "The recommended daily calorie intake for you is #{rec_modactive_calorie_upper} calories assuming a moderately active lifestyle."
     puts "To lose #{net_weight.round(3)}kg, you will need to burn an excess of #{calories.round(3)} calories."
     puts
-    exercise.each {|x, y| puts "#{x} 30 mins daily will burn #{y} calories, achieving your goal in #        {(calories/y).round(1)} days."}
+    exercise.each {|x, y| puts "#{x} 30 mins daily will burn #{y} calories, achieving your goal in #{(calories/y).round(1)} days."}
     puts
-    puts "Combined with a 500 calorie deficit diet, meaning #{rec_modactive_calorie_upper-500} calories daily, your     goal will be achieved in: "
-    exercise.each {|x, y| puts "#{x}: #{((calories/y).round(1)/2)} Days! Aprox. #{(((calories/y)/2)/30).round(1)}       Months!"}
+    puts "Combined with a 500 calorie deficit diet, meaning #{rec_modactive_calorie_upper-500} calories daily, your goal will be achieved in: "
+    exercise.each {|x, y| puts "#{x}: #{((calories/y).round(1)/2)} Days! Aprox. #{(((calories/y)/2)/30).round(1)}Months!"}
   else
     net_height = height - Math.sqrt(weight/25)
     net_weight = (18.5 * (height**2)) - weight
     puts "Please either shrink #{net_height.round(3)}m or gain #{net_weight.round(3)}kg."
     puts
-    puts "The recommended daily calorie intake for you is #{rec_modactive_calorie_lower} calories assuming a         moderately active lifestyle."
+    puts "The recommended daily calorie intake for you is #{rec_modactive_calorie_lower} calories assuming a moderately active lifestyle."
     puts "Please eat a surplus of 500 calories for a total of #{rec_modactive_calorie_lower+500} calories a day to      gain weight."
     puts
     puts "Here are some calorie guidelines: "
